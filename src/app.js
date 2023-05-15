f04256ace24b9c4854ea6692e4372e4217a94ee6
LeandroAgustinFernandez <leandroa.fernandez@gmail.com>
2023-05-15 16:57:09 -0300

    SegundaPreEntrega V1.0.0

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import cartRouter from "./routes/cart.router.js";
+import productsRouter from "./routes/products.router.js";
+import viewsRouter from "./routes/view.router.js";
+import handlebars from "express-handlebars";
+import express from "express";
+import cors from "cors";
+import { __dirname, PORT } from "./utils.js";
+import { Server } from "socket.io";
+import mongoose from "mongoose";
+
+const app = express();
+mongoose.connect(
+  "mongodb+srv://leandroafernandez:9WHsfQFziAzUm2LX@coderhousecluster.phsl88g.mongodb.net/ecommerce?retryWrites=true&w=majority"
+);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(`${__dirname}/public`));
+
+app.engine("handlebars", handlebars.engine());
+app.set("views", `${__dirname}/views`);
+app.set("view engine", "handlebars");
+
+app.use("/", viewsRouter);
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartRouter);
+
+const socketio = app.listen(PORT, () =>
+  console.log(`Server running at http://localhost:${PORT}`)
+);
+const io = new Server(socketio);
+app.set("socketio", io);
---
+++ src/routes/view.router.js
@@ -0,0 +1,43 @@
+import { Router } from "express";
+import ProductManager from "../dao/MongoDbManagers/ProductManager.js";
+import CartManager from "../dao/MongoDbManagers/CartManager.js";
+
+const router = Router();
+const productManager = new ProductManager("./products.json");
+const cartManager = new CartManager("./products.json");
+
+router.get("/", async (request, response) => {
+  response.render("index", {title: "Products", style: "home"});
+});
+
+router.get("/products", async (request, response) => {
+  const { limit, sort, page, query } = request.query;
+  const { docs, ...pag } = await productManager.getProducts(parseInt(limit), page, query, sort);
+  let urlParams = `?`;
+  if (query) urlParams += `query=${query}&`;
+  if (limit) urlParams += `limit=${limit}&`;
+  if (sort) urlParams += `sort=${sort}&`;
+  pag.prevLink = pag.hasPrevPage ? `${urlParams}page=${pag.prevPage}` : null;
+  pag.nextLink = pag.hasNextPage ? `${urlParams}page=${pag.nextPage}` : null;
+  response.render("products", { error: docs === undefined, products: docs, pag, title: "Products", style: "home" });
+});
+
+router.get('/product/:pid',async (request,response)=>{
+  let { pid } = request.params
+  let product = await productManager.getProductById(pid)
+  let error = product?.error ? true : false
+  response.render("productdetail", { error , product, title: `Product ${product.title}`, style: "home" });
+})
+
+router.get("/newproduct", async (request, response) => {
+  response.render("newproduct", {title: "Products", style: "home"});
+});
+
+router.get("/carts/:cid", async (request, response) => {
+  let { cid } = request.params
+  let cart = await cartManager.getCart(cid)
+  let { products, _id } = cart
+  response.render("carts", {title: "Products", style: "home", products ,_id});
+});
+
+export default router;
